37a14bf6a47179583542a5163f9cdf9c5dc9609d
Leonardo Brum <leonardobrum.dev@gmail.com>
2024-10-17 23:06:45 -0300

    debug envs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrlVolunteers: 'https://aprender-para-vida-api.onrender.com/volunteers',
-    apiUrlHelped: 'https://aprender-para-vida-api.onrender.com/helped',
+    apiUrlVolunteers: '',
+    apiUrlHelped: '',
 };
