2bf175cc91a9a9e7b802d3e9595666cacf201098
Luhh2007 <lulusan2007@gmail.com>
2024-11-03 13:53:50 -0300

    Teste barra de pesquisa

--- src/app/components/panel-header/panel-header.component.spec.ts
+++ src/app/components/panel-header/panel-header.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PanelHeaderComponent } from './panel-header.component';
+import { By } from '@angular/platform-browser';
 
 describe('PanelHeaderComponent', () => {
   let component: PanelHeaderComponent;
@@ -19,5 +19,17 @@ describe('PanelHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit search event when input value changes', () => {
+    spyOn(component.search, 'emit');
+
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    const testValue = 'test search';
+
+    inputElement.value = testValue;
+    inputElement.dispatchEvent(new Event('input'));
+
+    expect(component.search.emit).toHaveBeenCalledWith(testValue);
   });
 });
